00f510c9dab2bb4c22e44858ccd596f27ca369b3
Elvis on Arch2 <elvios@gmail.com>
2014-09-18 15:40:10 +0200

    converted to 4spaces instead of tabs, plus the BLOSUM file is inserted to matrix

--- me_gorilla_or_sea_cucumber.py
+++ me_gorilla_or_sea_cucumber.py
@@ -1,41 +1,37 @@
 import sys
 
 def load_matrix():
-	file = open('data/BLOSUM62.txt')
-	data = file.readlines()[6:]
-	file.close()
-	data = [d.strip() for d in data]
+    file = open('data/BLOSUM62.txt')
+    data = file.readlines()[6:]
+    file.close()
+    data = [d.strip() for d in data]
 
-	alphabet = data[0].split()
-	data = data[1:]
+    alphabet = data[0].split()
+    data = data[1:]
 
-	lookup = {}
-	for index, a in enumerate(alphabet):
-		lookup[a] = index
+    lookup = {}
+    for index, a in enumerate(alphabet):
+        lookup[a] = index
 
-	#print lookup['*'],lookup['A']
+    #print lookup['*'],lookup['A']
 
-	matrix = [[0 for i in range(0,23)] for j in range(0,23)]
-	
-	for k in matrix:
-		for l in k:
-			print l
+    matrix = [[0 for i in range(0,23)] for j in range(0,23)]
+    
+    for i in range(0,23):
+        numberlist = data[i].split()
+        for j in range(0,23):
+            matrix[j][i] = numberlist[j+1]
 
-	# for i,d in enumerate(data):
-	# 	matrix[i] = d.split()[1:]
-				
-	# print matrix[0][0]		
-
-	# values = data[1:]
-	# for v in values:
-	# 	for h in v.split():
-	# 		print h
-
-	
+    print matrix[0][0]
+    print matrix[0][1]
+    print matrix[0][2]
+    print matrix[0][3]
+    print matrix[0][4]
+    print matrix[0][5]
 
 
 def parse_data():
-	data = sys.stdin.read()
-	return data
+    data = sys.stdin.read()
+    return data
 
 load_matrix()
