086c726b7ee405887ee427e0885a12857058d710
René Anda Nielsen <anda83@gmail.com>
2014-09-23 11:58:15 +0200

    Done

--- mwek_alignment_solution.py
+++ mwek_alignment_solution.py
@@ -1,13 +1,14 @@
-import sys, itertools
+import sys
+import itertools
 
-M = {}
 score_matrix = {}
+
 
 def load_matrix():
     file_path = 'BLOSUM62.txt'
-    file = open(file_path)
-    data = file.readlines()[6:]
-    file.close()
+    input_file = open(file_path)
+    data = input_file.readlines()[6:]
+    input_file.close()
     data = [d.strip() for d in data]
 
     alphabet = data[0].split()
@@ -16,81 +17,79 @@ def load_matrix():
     for i, a in enumerate(alphabet):
         for values in data:
             listed_values = values.split()
-            score_matrix[a, listed_values[0]] = int(listed_values[i+1])
+            score_matrix[a, listed_values[0]] = int(listed_values[i + 1])
+
 
 def alignment(X, Y):
     i = len(X)
     j = len(Y)
 
-    if (i,j) in M:
-        return M[i,j]
+    if (i, j) in M:
+        return M[i, j]
 
-    delta = score_matrix['A','*']
+    delta = score_matrix['A', '*']
 
     if i == 0:
-        return '-'*j, Y, delta*j
+        return '-' * j, Y, delta * j
     if j == 0:
-        return X, '-'*i, delta*i
+        return X, '-' * i, delta * i
 
     X_i = X[-1]
     Y_j = Y[-1]
-    
+
     match_score = score_matrix[X_i, Y_j]
 
     recursive_call1 = alignment(X[:-1], Y[:-1])
-    result1 = recursive_call1[0] + X_i, recursive_call1[1] + Y_j,  match_score + recursive_call1[2]
-    
+    result1 = recursive_call1[0] + X_i, recursive_call1[1] + Y_j,\
+    match_score + recursive_call1[2]
+
     recursive_call2 = alignment(X[:-1], Y)
-    result2 = recursive_call2[0] + X_i, recursive_call2[1] + '-',  delta + recursive_call2[2]
-    
+    result2 = recursive_call2[0] + X_i, recursive_call2[1] + '-',\
+    delta + recursive_call2[2]
+
     recursive_call3 = alignment(X, Y[:-1])
-    result3 = recursive_call3[0] + '-', recursive_call3[1] + Y_j,  delta + recursive_call3[2]
+    result3 = recursive_call3[0] + '-', recursive_call3[1] + Y_j,\
+    delta + recursive_call3[2]
 
     max_result = get_max_tuple([result1, result2, result3])
 
-    M[i,j] = max_result
+    M[i, j] = max_result
 
     return max_result
 
 
 def get_max_tuple(A):
-    max_value = max(x[2] for x in A)    
+    max_value = max(x[2] for x in A)
     return [x for x in A if x[2] == max_value][0]
+
 
 def parse_data():
     data = sys.stdin.read().splitlines()
-    
+
     sequences = []
-    #sequences = {}
     sequence = ""
     name = ""
     first_sequence = True
     for d in data:
         if d[0] == '>':
             if not first_sequence:
-                sequences.append((name,sequence))
-                #sequences[name] = sequence
+                sequences.append((name, sequence))
             first_sequence = False
             name = d.split()[0][1:]
             sequence = ""
         else:
             sequence += d
-    sequences.append((name,sequence))
-    #sequences[name] = sequence
-    for s in sequences:
-        print s[1]
+    sequences.append((name, sequence))
     return sequences
 
 load_matrix()
 data = parse_data()
-a = "MVHLTPEEKSAVTALWGKVNVDEVGGEALGRLLVVYPWTQRFFESFGDLSTPDAVMGNPKVKAHGKKVLGAFSDGLAHLDNLKGTFATLSELHCDKLHVDPENFRLLGNVLVCVLAHHFGKEFTPPVQAAYQKVVAGVANALAHKYH"
-b = "MVHLTPEEKSAVTALWGKVNVDEVGGEALGRLLVVYPWTQRFFESFGDLSTPDAVMGNPKVKAHGKKVLGAFSDGLAHLDNLKGTFATLSELHCDKLHVDPENFKLLGNVLVCVLAHHFGKEFTPPVQAAYQKVVAGVANALAHKYH"
 
-#print alignment(a,b)
 #loop through possible combinations of sequences - disregarding ordering
-for sequence_1,sequence_2 in itertools.combinations(data, 2):
-    alignment_solution = alignment(sequence_1[1],sequence_2[1])
-    print sequence_1[0]+"--"+sequence_2[0]+": "+str(alignment_solution[2])
+for sequence_1, sequence_2 in itertools.combinations(data, 2):
+    M = {}
+    alignment_solution = alignment(sequence_1[1], sequence_2[1])
+    print sequence_1[0] + "--" + sequence_2[0] + ": " +\
+    str(alignment_solution[2])
     print alignment_solution[0]
     print alignment_solution[1]
-    
